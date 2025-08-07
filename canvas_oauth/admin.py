fe283a0c71dddd958ccd6ef943033196ef547ece
Patrick Tonne <patrick_tonne@harvard.edu>
2025-08-07 00:02:50 -0400

    Update admin configuration to include new `CanvasEnvironment` model

--- canvas_oauth/admin.py
+++ canvas_oauth/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
-from canvas_oauth.models import CanvasOAuth2Token
+
+from canvas_oauth.models import CanvasEnvironment, CanvasOAuth2Token
 
 
+@admin.register(CanvasEnvironment)
+class CanvasEnvironmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'domain', 'is_active', 'created_at')
+    list_filter = ('is_active', 'created_at')
+    search_fields = ('name', 'domain')
+    readonly_fields = ('created_at', 'updated_at')
+
+
+@admin.register(CanvasOAuth2Token)
 class CanvasOAuth2TokenAdmin(admin.ModelAdmin):
-    list_display = ('user', 'expires', 'created_on', 'updated_on')
-    search_fields = ('user',)
+    list_display = ('user', 'environment', 'expires', 'created_on', 'updated_on')
+    list_filter = ('environment', 'created_on')
+    search_fields = ('user__username', 'user__email', 'environment__name')
+    readonly_fields = ('created_on', 'updated_on')
 
-
-admin.site.register(CanvasOAuth2Token, CanvasOAuth2TokenAdmin)
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # Editing existing token
+            return self.readonly_fields + ('user', 'environment')
+        return self.readonly_fields
