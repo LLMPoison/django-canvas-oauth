6cbed71fa60536858f07c41e2304535cd06dd92e
griffinpup <christopherduhadway@gmail.com>
2025-10-22 15:04:20 -0400

    removed canvas_domain references

--- canvas_oauth/admin.py
+++ canvas_oauth/admin.py
@@ -5,12 +5,11 @@ from canvas_oauth.models import CanvasOAuth2Token
 
 @admin.register(CanvasOAuth2Token)
 class CanvasOAuth2TokenAdmin(admin.ModelAdmin):
-    list_display = ('user', 'canvas_domain', 'expires', 'created_on', 'updated_on')
-    list_filter = ('canvas_domain', 'created_on')
-    search_fields = ('user__username', 'user__email', 'canvas_domain')
+    list_display = ('user', 'expires', 'created_on', 'updated_on')
+    search_fields = ('user__username', 'user__email')
     readonly_fields = ('created_on', 'updated_on')
 
     def get_readonly_fields(self, request, obj=None):
         if obj:  # Editing existing token
-            return self.readonly_fields + ('user', 'canvas_domain')
+            return self.readonly_fields + ('user')
         return self.readonly_fields
